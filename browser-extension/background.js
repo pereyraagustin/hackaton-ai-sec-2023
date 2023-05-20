7d389662f6944ca73db8069960f473a83a76b52e
Gaston <gaston.herrerabaron@outlook.com>
2023-05-20 17:03:09 -0300

    Reemplazar dni por asteriscos

--- browser-extension/background.js
+++ browser-extension/background.js
@@ -11,14 +11,15 @@ function reddenPage() {
     .then(response => console.log(response))
     .catch(error => console.error('Error:', error)); */
     console.log( {"dnis":['12345678'],"entidades":["Juan","DNI"]})
-    let dnis = ['12345678'];
+    let dnis = ['12345678', '22345678'];
     let elementos = document.getElementsByTagName('body')[0].getElementsByTagName('*'); 
             
     for (var i = 0; i < elementos.length; i++) {
         var elemento = elementos[i];
         dnis.forEach(dni => {
           if (elemento.innerHTML.includes(dni)) {
-            elemento.innerHTML = elemento.innerHTML.replace(new RegExp(dni, 'g'), '*');
+            let reemplazo = '*'.repeat(dni.length)
+            elemento.innerHTML = elemento.innerHTML.replace(new RegExp(dni, 'g'), reemplazo);
         }
         });
         
